19bdd8003a32d2351c1d59082435545f94087230
devteph <devteph@gmail.com>
2023-02-03 05:31:53 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  ["*.{html,js}"],
+  theme: {
+    colors:{
+      blue1: '#003865',
+      blue2: '#05457e',
+      blue3: '#0c8ed1',
+      white: '#ffffff',
+      yellow: '#ffb91c',
+      yellow2: '#ffb202',
+      gray: '#8d8d8d',
+      gray2: '#f5f5f5',
+      sky1: '#47c9e5',
+      sky2: '#2c8b9e',
+      sky3: '#3AB3C9',
+      sky4: '#2D8C9E',
+      sky5: '#01c1d5',
+      orange: '#FFB200',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
